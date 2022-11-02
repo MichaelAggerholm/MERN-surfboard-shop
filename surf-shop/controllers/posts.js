0e7ef59c618bbfc6e638d6f970b4eaf2b90bdbc9
Michael Aggerholm <92aggerholm@gmail.com>
2022-11-02 21:00:56 +0100

    udarbejdelse af crud funktionalitet for posts.

--- surf-shop/controllers/posts.js
+++ surf-shop/controllers/posts.js
@@ -9,5 +9,16 @@ module.exports = {
     // Posts new
     newPost(req, res, next) {
         res.render('posts/new');
+    },
+    // Posts create
+    async createPost(req, res, next) {
+        // use req.body to create a new post
+        let post = await Post.create(req.body);
+        res.redirect(`/posts/${post.id}`);
+    },
+    // Posts show
+    async showPost(req, res, next) {
+        let post = await Post.findById(req.params.id);
+        res.render('posts/show', {post});
     }
 }
